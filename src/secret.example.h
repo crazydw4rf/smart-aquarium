8ebad79fc10cdff2edfccbae161def833eed68b2
Binar Nugroho <binarnugroho775@gmail.com>
2025-11-02 22:13:26 +0700

    hadeuh

--- src/secret.example.h
+++ src/secret.example.h
@@ -1,8 +1,8 @@
 #define WIFI_SSID "Foo Bar"
 #define WIFI_PASSWORD "foobar123"
 
-#define BOT_TOKEN "8466481443:AAHwk_CYqnGydtx2Zs9nPP3MYfy1feQx9xk"
-#define TELEGRAM_USER_ID "2136551679"
+#define BOT_TOKEN "Bot token disini"
+#define TELEGRAM_USER_ID "123456789"
 
 #define MQTT_USER "user"
 #define MQTT_PASSWORD "user123"
